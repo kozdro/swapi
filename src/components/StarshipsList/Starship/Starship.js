b0d5533ac04a22c1095e191ed807365f4fa885b0
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-04-26 22:09:03 +0200

    change fn to class

--- src/components/StarshipsList/Starship/Starship.js
+++ src/components/StarshipsList/Starship/Starship.js
@@ -1,32 +1,68 @@
-import { useEffect, useState } from "react";
+import React, { Component } from "react";
+import CartContext from "../../../context/cartContext";
 import "./Starship.css";
 
 const api = "https://swapi.dev/api/starships/";
 
-export default function Starship() {
-  const [starships, setStarships] = useState([]);
+class Starship extends Component {
+  static contextType = CartContext;
 
-  useEffect(() => {
+  state = {
+    starships: [],
+    value: 0,
+    quantity: 0,
+  };
+
+  componentDidMount() {
     fetch(api)
       .then((res) => res.json())
       .then((data) => {
-        setStarships(data.results);
+        this.setState({
+          starships: data.results,
+        });
       });
-  }, []);
+  }
 
-  return (
-    <tbody>
-      {starships.map((starship) => (
-        <tr>
-          <td>{starship.name}</td>
-          <td>{starship.manufacturer}</td>
-          <td>{starship.cost_in_credits}</td>
-          <td>
-            <input className="input" type="number" />
-            <button className="add-btn">Add</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  );
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleAddClick = () => {
+    this.setState({
+      quantity: this.state.value,
+    });
+  };
+
+  render() {
+    return (
+      <tbody>
+        {this.state.starships.map((starship) => (
+          <tr key={starship.url}>
+            <td>{starship.name}</td>
+            <td>{starship.manufacturer}</td>
+            <td>{starship.cost_in_credits}</td>
+            <td>
+              <input
+                key={starship.model}
+                onChange={this.handleInputChange}
+                value={this.value}
+                className="input"
+                type="number"
+                min="0"
+              />
+              <CartContext.Provider value={this.state.quantity}>
+                <button onClick={this.handleAddClick} className="add-btn">
+                  Add
+                </button>
+              </CartContext.Provider>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
 }
+
+export default Starship;
