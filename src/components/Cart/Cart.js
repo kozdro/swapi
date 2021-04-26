b987a9d9192e3ab99a7f29f8e688353ba677de5e
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-04-26 15:35:40 +0200

    change classname

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import "./Cart.css";
 export default function Cart() {
   return (
     <div className="cart-wrapper">
-      <i class="fas fa-shopping-cart"></i>
+      <i className="fas fa-shopping-cart"></i>
       <span className="quantity">2</span>
       <p>Cart</p>
     </div>
